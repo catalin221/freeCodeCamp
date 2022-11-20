bf9e31994e75d0a56d37a26f0699ceda20d63874
catalin221 <bejinariucatalin29@gmail.com>
2022-11-20 02:10:01 +0000

    build-a-world-cup-database

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,12 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+echo $($PSQL "truncate table teams, games")
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != year ]]
+  then
+    $PSQL "insert into games(year, round, winner_goals, opponent_goals) values ('$YEAR', '$ROUND', '$WINNER_GOALS', '$OPPONENT_GOALS');"
+  fi
+done
