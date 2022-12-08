d6adb2c28f474102a0ced3fef06238cfde359c36
catalin221 <bejinariucatalin29@gmail.com>
2022-12-08 14:25:12 +0000

    feat: Select data based on name when input is char/string

--- element.sh
+++ element.sh
@@ -8,12 +8,13 @@ then
 else
   if [[ $1 =~ ^[0-9]+$ ]]
   then
-    ELEMENT_FOUND=$($PSQL "select atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type from properties join elements using(atomic_number) join types using(type_id) where elements.atomic_number = $1")
-    echo $ELEMENT_FOUND
+    ELEMENT_FOUND=$($PSQL "select atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type from properties join elements using(atomic_number) join types using(type_id) where elements.atomic_number = $1;")
+  else
+    ELEMENT_FOUND=$($PSQL "select atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type from properties join elements using(atomic_number) join types using(type_id) where elements.symbol like '$1%' order by atomic_number LIMIT 1;")
   fi
 
   if [[ -z $ELEMENT_FOUND ]]
   then
-    echo "I could not find that element in the database"
+    echo "I could not find that element in the database."
   fi
 fi
