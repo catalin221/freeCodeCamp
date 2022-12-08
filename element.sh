2181a243b8e24184f740602d52b44aefadcf4a45
catalin221 <bejinariucatalin29@gmail.com>
2022-12-08 14:10:01 +0000

    feat: Select data based on atomic_number when input is a number

--- element.sh
+++ element.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
+
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
+else
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ELEMENT_FOUND=$($PSQL "select atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type from properties join elements using(atomic_number) join types using(type_id) where elements.atomic_number = $1")
+    echo $ELEMENT_FOUND
+  fi
+
+  if [[ -z $ELEMENT_FOUND ]]
+  then
+    echo "I could not find that element in the database"
+  fi
 fi
