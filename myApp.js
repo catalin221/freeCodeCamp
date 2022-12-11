332ad76d6ade2dc51270bb8f670921b330bb0349
unknown <bejinariucatalin29@gmail.com>
2022-12-11 22:33:30 +0200

    Fixed path

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 app.get("/", (req, res) => {
-    res.send(__dirname + views/index.html);
+    res.send(__dirname + "views/index.html");
 });
 
 
