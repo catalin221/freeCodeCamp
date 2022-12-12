7c79866d0b1c8c294e96c3fb2e3b1b14b5d6afea
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2022-12-12 16:01:05 +0200

    Add css file

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 let express = require('express');
 let app = express();
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
+app.use(express.static(__dirname + "/public/style.css"));
 
 
 
