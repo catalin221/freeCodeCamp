29cf18d73e3bcd0d96bf23c7fd27261bdabc204a
unknown <bejinariucatalin29@gmail.com>
2022-12-13 12:29:10 +0200

    Try

--- myApp.js
+++ myApp.js
@@ -1,10 +1,6 @@
 let express = require('express');
 let app = express();
-
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
-});
-app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
