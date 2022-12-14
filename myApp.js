45a67ccc38bb1242baf4c57a25443534868f20a5
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2022-12-14 23:29:13 +0200

    Add json path

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let app = express();
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
+app.get("/json", (req, res) => {
+    res.json({
+        message: "Hello json"
+    });
 });
 app.use("/public", express.static(__dirname + "/public"));
 
