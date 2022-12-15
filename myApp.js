e69c51e90e9f12f3347aa5cb6a383391e0ada642
unknown <bejinariucatalin29@gmail.com>
2022-12-15 22:47:38 +0200

    Fix

--- myApp.js
+++ myApp.js
@@ -1,11 +1,13 @@
 let express = require('express');
 let app = express();
+app.get("/", (req, res) => { res.sendFile(__dirname + "/views/index.html") });
+
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", (req, res) => {
     res.json({
         message: "Hello json"
     });
 });
-app.use("/public", express.static(__dirname + "/public"));
 
 
 
@@ -42,4 +44,4 @@ app.use("/public", express.static(__dirname + "/public"));
 
 
 
- module.exports = app;
+module.exports = app;
