9bf02f6e4e8f5aa31e4a401418048862d452945c
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2022-12-16 21:29:43 +0200

    Add env variable for message style

--- myApp.js
+++ myApp.js
@@ -1,12 +1,22 @@
+require('dotenv').config();
+
+const e = require('express');
 let express = require('express');
 let app = express();
 app.get("/", (req, res) => { res.sendFile(__dirname + "/views/index.html") });
 
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", (req, res) => {
-    res.json({
-        message: "Hello json"
-    });
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({
+            message: "Hello json".toUpperCase()
+        });
+    }
+    else {
+        res.json({
+            message: "Hello json"
+        });
+    }
 });
 
 
