6ec3cb5f6ccd56d5e7d3433ba025a5c0012f8723
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2022-12-16 23:01:00 +0200

    fix

--- myApp.js
+++ myApp.js
@@ -3,9 +3,16 @@ require('dotenv').config();
 const e = require('express');
 let express = require('express');
 let app = express();
+
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
+
 app.get("/", (req, res) => { res.sendFile(__dirname + "/views/index.html") });
 
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/json", (req, res) => {
     if (process.env.MESSAGE_STYLE === "uppercase") {
         res.json({
@@ -17,11 +24,6 @@ app.get("/json", (req, res) => {
             message: "Hello json"
         });
     }
-});
-
-app.use(function (req,res, next) {
-    console.log(req.method + " " + req.path + " - " + req.ip);
-    next();
 });
 
 
