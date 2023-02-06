a383620d3661035d260081045cd6d25940fafcaa
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2023-02-06 22:22:28 +0200

    fix

--- myApp.js
+++ myApp.js
@@ -20,8 +20,7 @@ const createAndSavePerson = (done) => {
   me.save(function(err, data) {
     if(err) return console.log(err);
     done(null, data);
-  })
-  done(null , data);
+  });
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
