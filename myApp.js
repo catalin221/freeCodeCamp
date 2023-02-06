fb1dd1b203cced92ecbc738c0a1122e93b715f8e
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2023-02-06 19:52:31 +0200

    Instantiate person fix

--- myApp.js
+++ myApp.js
@@ -15,6 +15,10 @@ mongoose.connect('mongodb+srv://catalin221:xqLpbr4ftyur7829@cluster0.qtwfddd.mon
 
 var Person = mongoose.model('Person', personSchema);
 
+let me = new Person({
+  name: "Paul"
+});
+
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
